d522174be88993bec23152cf179f792e4dc74cfd
Gaurav Sachdeva <iamarav>
2020-02-03 16:55:06 +0530

    Fixed issues

--- Django_Quora_Clone/settings.py
+++ Django_Quora_Clone/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import pymysql
+import MySQLdb
 
-pymysql.install_as_MySQLdb()
+#pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
